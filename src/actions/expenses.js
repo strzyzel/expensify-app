cbb72862730f26c7ae3919df954c9f1ce1722825
Łukasz Strzyżewski <l.j.strzyzewski@gmail.com>
2019-08-24 23:24:18 +0200

    Setup test database env

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,22 +1,17 @@
 import uuid from 'uuid';
-
-export const addExpense = (
-    { 
-        description = '', 
-        note = '', 
-        amount = 0, 
-        createdAt = 0} = {}
-    ) => ({
+import database from '../firebase/firebase';
+export const addExpense = (expense) => ({
     type: "ADD_EXPENSE",
-    expense: {
-        id: uuid(),
-        description,
-        note,
-        amount,
-        createdAt
-    }
+    expense
+    // : {
+    //     id: uuid(),
+    //     description,
+    //     note,
+    //     amount,
+    //     createdAt
+    // }
 });
-
+ 
 export const removeExpense = ({ id } = {}) => ({
     type: "REMOVE_EXPENSE",
     id
@@ -27,3 +22,22 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+export const startAddExpense = (expenseData = {}) => {
+    return (dispatch) => {
+        const {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+        } = expenseData;
+        const expense = { description, note, amount, createdAt };
+
+        return database.ref('expenses').push(expense).then((ref) => {
+        dispatch(addExpense({
+            id: ref.key,
+            ...expense
+        }));
+        });
+    };
+};
--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addExpense } from '../actions/expenses'
 import ExpenseForm from './ExpenseForm';
-
+import { startAddExpense } from '../actions/expenses';
 export class AddExpensePage extends React.Component {
     onSubmit=(expense) => {
-        this.props.addExpense(expense);
+        this.props.startAddExpense(expense);
         this.props.history.push("/");
     };
     render() {
@@ -17,6 +17,6 @@ export class AddExpensePage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addExpense: (expense) => dispatch(addExpense(expense))
+    startAddExpense: (expense) => dispatch(startAddExpense(expense))
 });
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
