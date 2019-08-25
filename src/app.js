f89d1138e460ac739caad536913273bb1f9734cc
Łukasz Strzyżewski <l.j.strzyzewski@gmail.com>
2019-08-25 14:48:46 +0200

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore';
+import LoadingPage from './components/LoadingPage';
 import { login, logout } from './actions/auth';
 import 'normalize-css/normalize.css';
 import './styles/styles.scss';
@@ -25,9 +26,7 @@ const renderApp = () => {
     }
 }
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
-
-
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged(user => {
     if(user) { 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,17 +3,25 @@ import { connect } from 'react-redux';
 import ExpenseItem from './ExpenseItem';
 import selectExpenses from '../selectors/expenses';
 export const ExpenseList = (props) => (
-    <div>
-        <h1>Expense List</h1>
-        {
-            props.expenses.length === 0 ? (
-                <p>No expenses</p>
-            ) : (
-                props.expenses.map((expense) => { 
-                    return <ExpenseItem {...expense} key={expense.id}/>; 
-                })
-            )
-        }
+    <div className="content-container">
+        <div className="list-header">
+            <div className="show-for-mobile">Expenses</div>
+            <div className="show-for-desktop">Expense</div>
+            <div className="show-for-desktop">Amount</div>
+        </div>
+        <div className="list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <div className="list-item list-item--message">
+                        <span>No expenses</span>
+                    </div>  
+                ) : (
+                    props.expenses.map((expense) => { 
+                        return <ExpenseItem {...expense} key={expense.id}/>; 
+                    })
+                )
+            }
+        </div>
     </div>
 );
 
